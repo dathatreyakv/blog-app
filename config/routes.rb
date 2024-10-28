2c1b97fbd3c9ced921a4aeafe23e68084926df2f
Venkata Dathatreya K <dathatreyakv@gmail.com>
2024-10-29 01:13:06 +0530

    create user and show user UI

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root 'welcome#home'
   get 'about', to: 'welcome#about'
 
-
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   resources :articles
 end
